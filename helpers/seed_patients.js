2d8efaa24f9ec8aef20e34bcd7801eb6586084a5
Oriol Hilari <orihil@icloud.com>
2018-10-18 18:48:01 +0200

    Doing merge

--- helpers/seed_patients.js
+++ helpers/seed_patients.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 const Patient = require('../models/patient.js');
-
-const saltRounds = 10;
 
 mongoose.connect('mongodb://hilaring:Patata12*@ds225703.mlab.com:25703/ih-project-3', { useNewUrlParser: true })
 
@@ -10,30 +7,11 @@ Patient.collection.drop();
 
 const patients = [
   {
-    name: 'Pere',
-    last_name: 'Busquet',
-    email: 'pere@pere.pere',
-    number: 666666666,
-    adress: 'Pamplona, 96, Barcelona, Catalunya',
-    visit: [{
-        Date: 1990-10-15,
-        Syntoms: 'Mal de cap',
-        Disease: 'cuentitis',
-        Prescription: 'paracetamol',
-      }, {
-        timestamps: {
-          createdAt: 'created_at',
-          updatedAt: 'updated_at'
-        },
-    }],
-  },
-  {
     name: 'Oriol',
     last_name: 'Hilari',
     email: 'oriol@hilari.com',
     number: 666666663,
     adress: 'Pamplona, 96, Barcelona, Catalunya',
-    visit: [],
   },
   {
     name: 'Thor',
@@ -41,7 +19,6 @@ const patients = [
     email: 'thor@spain.es',
     number: 12102018,
     adress: 'Pamplona, 96, Barcelona, Spain',
-    visit: [],
   },
   {
     name: 'Manu',
@@ -49,7 +26,6 @@ const patients = [
     email: 'pokemon@spain.es',
     number: 666123456,
     adress: 'Olesa de Montserrat, Catalunya, Spain',
-    visit: [],
   },
   {
     name: 'Mr. Potato',
@@ -57,7 +33,6 @@ const patients = [
     email: 'potato@toystory.com',
     number: 666666666,
     adress: 'Pamplona, 96, Barcelona, Catalunya',
-    visit: [],
   },
 ];
 
